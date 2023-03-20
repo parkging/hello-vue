024fffbe31c05086304b440893eb4b7d3ec6a625
parkging <parkjun611@gmail.com>
2023-03-20 17:28:03 +0900

    toast ui add

--- src/components/section/toastui/ToastEditor.vue
+++ src/components/section/toastui/ToastEditor.vue
@@ -1,107 +1,40 @@
 <template>
-  <div class="toast-edit" id="editor" ref="editor"></div>
+  <div class="toast-edit" id="editor"></div>
 </template>
 
 <script>
-// import "prismjs/themes/prism.css";
-// import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
-// import "tui-color-picker/dist/tui-color-picker.css";
-// import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
+import Editor from '@toast-ui/editor'
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all'
+import Prism from 'prismjs'
 
-// import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
-// import Prism from "prismjs";
-// import "prismjs/components/prism-c";
-// import "prismjs/components/prism-cpp";
-// import "prismjs/components/prism-java";
-// import "prismjs/components/prism-python";
-// import "@toast-ui/editor/dist/i18n/ko-kr";
-// import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
-// import Editor from "@toast-ui/editor";
-// import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight";
-
-// import 'codemirror/lib/codemirror.css'; // codemirror 스타일
-import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
-import Editor from '@toast-ui/editor';
-import 'highlight.js/styles/github.css'; // code block highlight 스타일
-
-// toast ui 설치 시 highlight js 역시 자동으로 설치된다.
-import codeSyntaxHightlight from '@toast-ui/editor-plugin-code-syntax-highlight';
-import hljs from 'highlight.js';
+import '@toast-ui/editor/dist/toastui-editor.css' // Editor's Style
+import 'prismjs/themes/prism.css'
 
 export default {
-  components: {
-  },
+  components: {},
   props: {
-    data: {
-      type: String,
-      default: "",
-    },
-   
+    data: {}
   },
   data() {
     return {
-      changedPrism: null,
       editor: null,
-     
-    };
-  },
-  mounted() {
-    // this.changedPrism = Prism;
-
-    this.setEditor();
-    if (this.data) {
-      try {
-        this.editor.setMarkdown(this.data);
-      } catch (e) {
-        this.editor.setHTML(this.data);
-      }
+      content: '동해물과 백두산이 마르고 닳도록'
     }
   },
-  methods: {
-    setEditor() {
-      this.editor = new Editor({
-        // el: document.querySelector("#editor"),
-        el: this.$refs.editor,
-      plugins: [[codeSyntaxHightlight, { hljs }]],
-        // plugins: [
-        // codeSyntaxHightlight
-        // //   // colorSyntax,
-        // //   // [codeSyntaxHighlight, { highlighter: this.changedPrism }]
-        // ],
-        initialEditType: "markdown",
-      // previewStyle: "tab",
-      // height: "auto",
-        // language: "ko",
-        // events: {
-        //   change: this.onChangeEditor,
-        // },
-        // hooks: {
-        //   addImageBlobHook: this.addImageBlobHook,
-        // },
-      });
-    },
-
-    // onChangeEditor() {
-    //   this.editorText = this.editor.getMarkdown();
-    // },
+  mounted() {
+    this.editor = new Editor({
+      el: document.querySelector('#editor'),
+      plugins: [[codeSyntaxHighlight, { highlighter: Prism }]],
+      initialEditType: 'markdown',
+      previewStyle: 'tab',
+      hideModeSwitch: true,
+      height: 'auto',
+      initialValue: this.content
+    })
   },
-  watch: {
-    // // watch를 활용한 props 변경 감지
-    // data(newValue) {
-    //   if (this.editorText !== newValue) {
-    //     this.editor.setMarkdown(newValue);
-    //   }
-    // },
-
-    // editorText(newValue, oldValue) {
-    //   if (newValue !== oldValue) {
-    //     this.$emit("setContent", this.editor.getMarkdown());
-    //   }
-    // },
-  },
-};
+  methods: {},
+  watch: {}
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/section/toastui/ToastViewer.vue
+++ src/components/section/toastui/ToastViewer.vue
@@ -1,38 +1,38 @@
 <template>
-  <div v-if='article'>
-    <div id="viewer"/>
-  </div>
+  <div id="viewer"></div>
 </template>
+
 <script>
-import '@toast-ui/editor/dist/toastui-editor-viewer.css';
-import 'highlight.js/styles/github.css';
-import Viewer from '@toast-ui/editor/dist/toastui-editor-viewer';
-import codeSyntaxHightlight from '@toast-ui/editor-plugin-code-syntax-highlight';
-import hljs from 'highlight.js';
+import Viewer from '@toast-ui/editor/dist/toastui-editor-viewer'
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all'
+import Prism from 'prismjs'
+
+import '@toast-ui/editor/dist/toastui-editor.css' // Editor's Style
+import 'prismjs/themes/prism.css'
 
 export default {
-  components: {}, 
+  components: {},
+  props: {
+    data: {}
+  },
   data() {
-  return {
-      sampleData: ''
+    return {
+      viewer: null,
+      content: '# 동해물과 백두산이 마르고 닳도록\ndddd'
     }
   },
-  setup() {}, 
-  created() {}, 
-  mounted: {
-  getViewer() {
-      this.getArticle().then((res) => {
-        new Viewer({
-          el: document.querySelector('#viewer'),
-          // 표시하고자 하는 내용은 여기에 들어간다.
-          initialValue: res,
-          plugins: [[codeSyntaxHightlight, { hljs }]],
-        });
-      });
-    },
-},
-
-  unmounted() {},
-  methods: {}
+  mounted() {
+    this.viewer = new Viewer({
+      el: document.querySelector('#viewer'),
+      viewer: true,
+      plugins: [[codeSyntaxHighlight, { highlighter: Prism }]],
+      height: 'auto',
+      initialValue: this.content
+    })
+  },
+  methods: {},
+  watch: {}
 }
 </script>
+
+<style></style>
